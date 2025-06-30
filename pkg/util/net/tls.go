31cb80d7f4076bf0e66b1cefcb9647cd48cdd235
lengerqi <lengerqi@126.com>
2025-06-30 11:41:17 +0800

    Update tls.go

--- pkg/util/net/tls.go
+++ pkg/util/net/tls.go
@@ -23,13 +23,13 @@ import (
 	libnet "github.com/fatedier/golib/net"
 )
 
-var FRPTLSHeadByte = 0x17
+var FRPTLSHeadByte = 0x10
 
 func CheckAndEnableTLSServerConnWithTimeout(
 	c net.Conn, tlsConfig *tls.Config, tlsOnly bool, timeout time.Duration,
 ) (out net.Conn, isTLS bool, custom bool, err error) {
-	sc, r := libnet.NewSharedConnSize(c, 2)
-	buf := make([]byte, 1)
+	sc, r := libnet.NewSharedConnSize(c, 4)
+	buf := make([]byte, 3)
 	var n int
 	_ = c.SetReadDeadline(time.Now().Add(timeout))
 	n, err = r.Read(buf)
@@ -39,11 +39,11 @@ func CheckAndEnableTLSServerConnWithTimeout(
 	}
 
 	switch {
-	case n == 1 && int(buf[0]) == FRPTLSHeadByte:
+	case n == 3 && int(buf[0]) == FRPTLSHeadByte:
 		out = tls.Server(c, tlsConfig)
 		isTLS = true
 		custom = true
-	case n == 1 && int(buf[0]) == 0x16:
+	case n == 3 && int(buf[0]) == 0x16:
 		out = tls.Server(sc, tlsConfig)
 		isTLS = true
 	default:
