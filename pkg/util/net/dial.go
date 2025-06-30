386baa6f28fbc2e6652d4e4f2848459e1063884a
lengerqi <lengerqi@126.com>
2025-06-30 11:59:53 +0800

    Update dial.go

--- pkg/util/net/dial.go
+++ pkg/util/net/dial.go
@@ -12,7 +12,8 @@ import (
 func DialHookCustomTLSHeadByte(enableTLS bool, disableCustomTLSHeadByte bool) libnet.AfterHookFunc {
 	return func(ctx context.Context, c net.Conn, addr string) (context.Context, net.Conn, error) {
 		if enableTLS && !disableCustomTLSHeadByte {
-			_, err := c.Write([]byte{byte(FRPTLSHeadByte)})
+			//_, err := c.Write([]byte{byte(FRPTLSHeadByte)})
+			_, err := c.Write([]byte{byte(FRPTLSHeadByte), byte(0x61), byte(0x63)})
 			if err != nil {
 				return nil, nil, err
 			}
